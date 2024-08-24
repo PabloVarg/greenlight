2d65f1a59017b53d6a07491794078534cac628ec
Pablo <49590991+PabloVarg@users.noreply.github.com>
2024-08-23 19:54:37 -0600

    feat(movies): Sort results

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,11 @@
 package data
 
-import "greenlight.pvargasb.com/internal/validator"
+import (
+	"slices"
+	"strings"
+
+	"greenlight.pvargasb.com/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +21,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	if slices.Contains(f.SortSafeList, f.Sort) {
+		return strings.TrimPrefix(f.Sort, "-")
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
